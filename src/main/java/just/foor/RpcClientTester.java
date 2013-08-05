88cc4d91d22716d146c21030304c963e0a9032df
RisingV <francis.yjj@gmail.com>
2013-08-05 15:48:01 +0800

    just test

--- src/main/java/just/foor/RpcClientTester.java
+++ src/main/java/just/foor/RpcClientTester.java
@@ -30,9 +30,18 @@ public class RpcClientTester implements ApplicationConstants {
         ITesterAPI it = new RpcClient( serverConfig );
         CommuStatus cs = it.connectToServer( "172.16.22.202" );
         CardInfo cardInfo = it.getCardInfo(cs.getSocketId(), 1);
+        int status0 = it.disconnectToServer( cs.getSocketId() );
+        CommuStatus cs2 = it.connectToServer( "172.16.22.202" );
+        int status1 = it.disconnectToServer( cs2.getSocketId() );
 
         System.out.println( "isConnected: " + cs.isConnected() );
         System.out.println( "socketID: " + cs.getSocketId() );
+
+        System.out.println( "2 isConnected: " + cs2.isConnected() );
+        System.out.println( "2 socketID: " + cs2.getSocketId() );
+
+        System.out.println( "status0: " + status0 );
+        System.out.println( "status1: " + status1 );
 
         System.out.println( "cardID:" + cardInfo.getCardId() );
         System.out.println( "cardType:" + cardInfo.getCardType() );
